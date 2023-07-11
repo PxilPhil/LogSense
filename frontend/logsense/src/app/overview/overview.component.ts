64128400ee708ad6ca249ffe4bb0639c6ca2e106
20190040 <20190040@students.htl-perg.ac.at>
2023-07-11 11:27:10 +0200

    wip

---
+++ frontend/logsense/src/app/overview/overview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.scss']
+})
+export class OverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
