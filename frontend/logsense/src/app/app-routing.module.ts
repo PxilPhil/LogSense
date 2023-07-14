fac02922762b681676ed6b0b51e93891e95b30e6
20190040 <20190040@students.htl-perg.ac.at>
2023-07-14 19:58:18 +0200

    new angular project

--- frontend/logsense/src/app/app-routing.module.ts
+++ frontend/logsense/src/app/app-routing.module.ts
@@ -1,25 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MainPageComponent} from "./main-page/main-page.component";
-import {OverviewComponent} from "./overview/overview.component";
-import {SingleProcessComponent} from "./single-process/single-process.component";
-import {RamComponent} from "./ram/ram.component";
-import {CpuComponent} from "./cpu/cpu.component";
-import {DiskComponent} from "./disk/disk.component";
-import {NetworkComponent} from "./network/network.component";
-import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [
-  { path: '', component: MainPageComponent},
-  { path: 'main', component: MainPageComponent},
-  { path: 'overview', component: OverviewComponent},
-  { path: 'processes', component: SingleProcessComponent},
-  { path: 'ram', component: RamComponent},
-  { path: 'cpu', component: CpuComponent},
-  { path: 'disk', component: DiskComponent},
-  { path: 'network', component: NetworkComponent},
-  { path: 'login', component: LoginComponent},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/logsense/src/app/app.module.ts
+++ frontend/logsense/src/app/app.module.ts
@@ -3,51 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { HeaderComponent } from './header/header.component';
-import { OverviewComponent } from './overview/overview.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatGridListModule} from "@angular/material/grid-list";
-import { AlertsComponent } from './alerts/alerts.component';
-import { ResourceMetricsComponent } from './resource-metrics/resource-metrics.component';
-import { TimeMetricsComponent } from './time-metrics/time-metrics.component';
-import { SingleProcessComponent } from './single-process/single-process.component';
-import { CpuComponent } from './cpu/cpu.component';
-import { RamComponent } from './ram/ram.component';
-import { DiskComponent } from './disk/disk.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import {MatIconModule} from "@angular/material/icon";
-import { NetworkComponent } from './network/network.component';
-import { LoginComponent } from './login/login.component';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainPageComponent,
-    ToolbarComponent,
-    HeaderComponent,
-    OverviewComponent,
-    AlertsComponent,
-    ResourceMetricsComponent,
-    TimeMetricsComponent,
-    SingleProcessComponent,
-    CpuComponent,
-    RamComponent,
-    DiskComponent,
-    DashboardComponent,
-    NetworkComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatGridListModule,
-    MatIconModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
