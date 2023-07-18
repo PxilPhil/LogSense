3c1940a0eb72e4ffcb197f99c298cb9c30cf38b4
20190040 <20190040@students.htl-perg.ac.at>
2023-07-18 12:51:58 +0200

    wip - cpu general info

--- frontend/logsense/src/app/cpu/cpu.component.ts
+++ frontend/logsense/src/app/cpu/cpu.component.ts
@@ -3,6 +3,21 @@ import {Chart} from "chart.js";
 import _default from "chart.js/dist/plugins/plugin.legend";
 import labels = _default.defaults.labels;
 
+export class CPUModel {
+  cpuName: String = "AMD Ryzen 7 5800H";
+  identifier: String = "Intel64 Family 6 Model 165 Stepping 2";
+  processorID: String = "BFEBFBFF000A0652";
+  vendor: String = "GenuineIntel";
+  bitness: String = "64 Bit";
+  physicalPackages: Number = 1;
+  physicalProcessors: Number = 6;
+  logicalProcessors: Number = 12;
+  contextSwitches: String = "230431317";
+  interrupts: String = "185631654";
+  current: Number = 21; //%
+  average: Number = 48; //%
+  stability: String = "Low";
+}
 @Component({
   selector: 'app-cpu',
   templateUrl: './cpu.component.html',
@@ -10,6 +25,12 @@ import labels = _default.defaults.labels;
 })
 export class CpuComponent implements OnInit{
 
+  cpu: CPUModel = new CPUModel();
+  average: number = 21;
+  current: number = 48;
+  stability: String = "Low";
+  selectedTime: String = "Last 24h";
+  notes: String[] = ["CPU Usage dropped 4%", "21 Anomalies detected", "5 Events registered"]
   constructor() {
   }
   ngOnInit() {
