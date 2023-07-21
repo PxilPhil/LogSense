f9a850da991b7254437069df4a85c16f31555da0
20190040 <20190040@students.htl-perg.ac.at>
2023-07-21 13:14:07 +0200

    wip - processes screen, single process

--- frontend/logsense/src/app/single-processes/single-processes.component.ts
+++ frontend/logsense/src/app/single-processes/single-processes.component.ts
@@ -34,4 +34,5 @@ export class SingleProcessesComponent {
       {name: "Chrome", processID: "5136", path: "C:\\Windows\\System\\chrome.exe", workingDirectory: "C:\\WINDOWS\\sytem32\\", cmdLn: "C:\\etc...", parentProcessID: "1148", user: "sarah", bitness: 64, cpuAverage: 24, ramAverage: 45, runtime: 69, majorFaults: 0, minorFaults: 28177, contextSwitches: 0, threads: 4, openFiles: 489, statistics: ["CPU Usage dropped 4%", "21 anomalies detected", "5 Events registered", "Rise of RAM usage of 90% detected"], alerts: ["Abnormal RAM-Spikes detected", "Memory leak possible"]},
       {name: "Vmmem", processID: "1456", path: "C:\\Windows\\System\\svchost.exe", workingDirectory: "C:\\WINDOWS\\sytem32\\", cmdLn: "C:\\etc...", parentProcessID: "1148", user: "sarah", bitness: 64, cpuAverage: 24, ramAverage: 45, runtime: 69, majorFaults: 0, minorFaults: 28177, contextSwitches: 0, threads: 4, openFiles: 489, statistics: ["CPU Usage dropped 4%", "21 anomalies detected", "5 Events registered", "Rise of RAM usage of 90% detected"], alerts: ["Abnormal RAM-Spikes detected", "Memory leak possible"]}
     ];
+    selectedProcess: Process = new Process();
 }
