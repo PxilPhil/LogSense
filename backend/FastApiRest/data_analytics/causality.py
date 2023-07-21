046f0ef26e8d006d01622229e47d4f6da61f18cd
Borbely <philipp.borbely@dynatrace.com>
2023-07-21 12:39:50 +0200

    Migrated data analytics and added more endpoints

--- backend/FastApiRest/data_analytics/causality.py
+++ backend/FastApiRest/data_analytics/causality.py
@@ -1,16 +1,16 @@
 causation_limit = 0.7  # only check overlap for applications over this causation limit (max is 1)
-causation_percentual_limit = 0.1  # only return data with a percentual influence of this value to the total usage
+causation_percentual_limit = 0.3  # only return data with a percentual influence of this value to the total usage
+relevancy_limit = 0.05
 application_list = []  # list of applications with high causality to total usage
 
 
-def detect_causality(timestamp_df, agg_df, df,
-                     column):  # alternatively just work with the processes with the largest usage
-    for index, row in agg_df.iterrows():
-        curr_row = df.loc[df.name == index].groupby(['timestamp']).sum(numeric_only=True).sort_values(by=['timestamp'])
-        corr = timestamp_df[column].corr(curr_row[column])  # find out causality
-        if corr > causation_limit:
-            print('causality: ')
-            print(index)
+def detect_relevancy(timestamp_df, df, column):
+
+    pc_mean = timestamp_df[column].mean()
+    application_mean = df.groupby("name").mean(numeric_only=True)
+    for index, row in application_mean.iterrows():
+        mean = row[column]
+        if mean/pc_mean > relevancy_limit:
             application_list.append(index)
     return detect_causality_percentual(df, timestamp_df, column)
 
@@ -19,19 +19,20 @@ def detect_causality_percentual(df, timestamp_df, column):
     causality_map = dict()
     # calculate how much in percentual values the delta of applications and
     # total ram usage overlap
-    timestamp_df['Delta'] = timestamp_df[column].diff()
-    timestamp_df['Delta'].fillna(1, inplace=True)
+    timestamp_df['Delta'] = timestamp_df[column].diff().dropna()
 
     for application in application_list:
         application_row = df.loc[df.name == application].groupby(['timestamp']).sum(numeric_only=True).sort_values(
             by=['timestamp'])
-        application_row['Delta'] = application_row[column].diff()
-        application_row['Delta'].fillna(1, inplace=True)
+
+        application_row['Delta'] = application_row[column].diff().dropna()
 
         causality_list = []
         for index, row in application_row.iterrows():
             causality_list.append(abs(row['Delta'] / timestamp_df.loc[index]['Delta']))  # abs because negative values
-        causality_map[application] = sum(causality_list) / len(causality_list)
+
+        if len(causality_list) > 0:
+            causality_map[application] = sum(causality_list) / len(causality_list)
 
     return convert_to_list(causality_map)
 
--- backend/FastApiRest/data_analytics/manipulation.py
+++ backend/FastApiRest/data_analytics/manipulation.py
@@ -1,5 +1,8 @@
+import pandas as pd
+import numpy as np
 
-def preprocess_data(df):
+
+def group_by_timestamp(df):
     return df.groupby(['timestamp']).sum(numeric_only=True).sort_values('timestamp', ascending=True)
 
 
@@ -8,11 +11,25 @@ def select_rows_by_application(selected_value, df):  # Method to select rows by 
         by=['timestamp'])
 
 
-def calculate_moving_avg(rows, column):  # calculates moving avg for the column
-    rows['MovingAvg'] = rows[column].rolling(
+def calculate_moving_avg(df, column):  # calculates moving avg for the column
+    df['MovingAvg'] = df[column].rolling(
         window=5).mean()  # moving averages can be used to flatten
-    return rows
+    df = df.dropna()
+    return df
 
 
 def aggregate(df):
     return df.groupby("name").agg(list)
+
+
+def create_df_between(start_timestamp, end_timestamp, frequency):
+    start_datetime = pd.to_datetime(start_timestamp, unit='ms')
+    end_datetime = pd.to_datetime(end_timestamp, unit='ms')
+
+    timestamp_index = pd.date_range(start=start_datetime, end=end_datetime, freq=frequency)
+    timestamp_values = timestamp_index.astype(np.int64) // 10 ** 6
+
+    new_df = pd.DataFrame(index=timestamp_values)
+
+    return new_df
+
