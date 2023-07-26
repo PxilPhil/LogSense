fb80c3290644f9d1ce6083215729ecbeacc38500
20190051 <20190051@students.htl-perg.ac.at>
2023-07-26 13:22:46 +0200

    Added more comments and ToDos

--- backend/FastApiRest/data_analytics/requests.py
+++ backend/FastApiRest/data_analytics/requests.py
@@ -7,10 +7,10 @@ from data_analytics.prediction import fit_linear_regression, predict_for_df
 from data_analytics.helper import read_csv, calc_end_timestamp
 
 warnings.filterwarnings("ignore")
-first_ts = 0
 df = pd.DataFrame()  # current data frame
 
 def ingest_process_data(new_df):
+    #TODO: We could already detect a lot of useful data on ingest, find out if we leave it at a hybrid approach or ingest
     global df
     new_df = new_df.set_index('timestamp')
     anomaly_map = dict()
@@ -44,36 +44,34 @@ def predict_resource_data(df, days):
     return df
 
 
-def fetch_application_data(name):  # supposed to analyze trends and everything in detail for one certain application
-    df = read_csv(
-        os.path.join("C:/Users/philipp.borbely/Documents/LogSenseRepo/logsense/backend/FastApiRest/data/processes",
-                     "*.csv"), False)
-    df = manipulation.select_rows_by_application(name, df)
-    print(df)
+def fetch_application_data(df):  # supposed to analyze trends and everything in detail for one certain application
     # find trends
     df = manipulation.calculate_moving_avg(df, 'residentSetSize')
     trend_list = trend.detect_trends(df, 'MovingAvg')
     # find anomalies
     anomaly_list = anomaly.detect_anomalies(df, 'residentSetSize')
-    return df, anomaly_list, trend_list
-
-
-def fetch_pc_data(df):  # fetch all application data in database for certain time period and currently only set for ram
-    pc_total_df = manipulation.group_by_timestamp(df) #TODO: We can skip this if we get pc_total_data itself from database
-    print(pc_total_df)
-    # get influence percentage
-    involvement_map = involvement.detect_involvement_percentual(df, pc_total_df, 'residentSetSize')
-    # find trends
-    pc_total_df = manipulation.calculate_moving_avg(pc_total_df, 'residentSetSize')
-    trend_list = trend.detect_trends(pc_total_df, 'MovingAvg')
     # get stats
     std = df['residentSetSize'].std()
     mean = df['residentSetSize'].mean()
+    return df, anomaly_list, trend_list, std, mean
+
+
+def fetch_pc_data(df, column):  # fetch all application data in database for certain time period and currently only set for ram
+    pc_total_df = manipulation.group_by_timestamp(df) #TODO: We can skip this if we get pc_total_data itself from database
+    print(pc_total_df)
+    # get influence percentage
+    involvement_map = involvement.detect_involvement_percentual(df, pc_total_df, column)
+    # find trends
+    pc_total_df = manipulation.calculate_moving_avg(pc_total_df, column)
+    trend_list = trend.detect_trends(pc_total_df, 'MovingAvg')
+    # get stats
+    std = df[column].std()
+    mean = df[column].mean()
     # get list of relevant data
-    application_list = involvement.detect_relevancy(pc_total_df, df, 'residentSetSize')
+    application_list = involvement.detect_relevancy(pc_total_df, df, column)
     # get allocation percentage
     df = df.set_index(['timestamp'])
     latest_row = df[df.index == df.index.max()]
-    allocation_map = stats.calc_allocation(latest_row, 'residentSetSize', application_list)
+    allocation_map = stats.calc_allocation(latest_row, column, application_list)
     # TODO: compare current value with last
     return pc_total_df, allocation_map, std, mean, trend_list, involvement_map
