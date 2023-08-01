7f36997784e311b70958731c8f78afc3e6c71d10
20190051 <20190051@students.htl-perg.ac.at>
2023-08-01 17:20:41 +0200

    Added CORS for angular to be accesed

--- backend/FastApiRest/main.py
+++ backend/FastApiRest/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.data import data
 from api.group import group
 from api.pc import pc
 from api.user import user
 from api.application import application
-
 
 app = FastAPI()
 
@@ -32,6 +32,19 @@ tags_metadata = [
     }
 ]
 
+# TODO: Add origin of angular application
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user, tags=["User"], prefix="/user")
 app.include_router(group, tags=["Group"], prefix="/group")
 app.include_router(pc, tags=["PC"], prefix="/pc")
@@ -40,7 +53,9 @@ app.include_router(data, tags=["Data"], prefix="/data")
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
+
 
 @app.get("/")
 def root():
